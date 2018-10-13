349e6bfac23d929db48145623ac07139e10a9a17
webwaffle <sam525watchman@gmail.com>
2018-10-13 15:30:58 -0400

    Almost Finished Login System

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-
+var fs = require('fs');
 
 app.use(require('express').static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
@@ -41,13 +41,25 @@ app.get('/', (req, res) => {
     res.render('pages/login')
 })
 
-app.get('/login-process', (req, res) => {
+app.post('/login-process', (req, res) => {
     var table = fileToJson('data/families.json');
     for(var i = 0; i < table.length; i++) {
-        
+        if(table[i].username == req.body.username) {
+            if(table[i].password == req.body.password) {
+                req.session.username = table[i].username;
+                res.redirect('/home');
+                return;
+            } else {
+                res.render('messages/wrongPassword');
+                return;
+            }
+        }
     }
+    res.render('messages/wrongUsername');
+    return;
 })
 
 app.listen(3000, () => {
     console.log('Started on port 3000')
 })
+
